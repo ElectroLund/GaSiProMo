e829f13d2401cea82fcbb88270d758ca6c9a6425
Rob <rob.f.lund+github@gmail.com>
2015-11-06 22:25:16 -0700

    sample code
    didn't need this file for project

--- OsRAM_Arduino/sample code.c
+++
@@ -1,8 +0,0 @@
-#define MAX_ADDRESS_LINES		6
-
-#define DISPLAY_STRING (const char[MAX_ADDRESS_LINES])	\
-{																			\
-	1,	1, 1, 0,	1, 0													\	
-}
-
-const byte DISPLAY_STRING[] = { 1, 1, 1, 0, 1, 0 };
